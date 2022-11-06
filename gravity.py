2233b6f57f740cbd865942eec3c8dae846df74c5
Dmitry M <38764934+volemor@users.noreply.github.com>
2022-11-06 16:14:26 +0300

    start gravity test

---
+++ gravity.py
@@ -0,0 +1,117 @@
+import random
+import datetime
+import pygame
+import time
+import math
+
+
+weght, heght = 1000,1000
+pygame.init()
+screen = pygame.display.set_mode((weght,heght))
+RED = (225, 0, 0)
+BLACK = (0, 0, 0)
+timer = 0
+mass_point = []  # pos_x,pos_y, deltaV_x, deltaV_y, mass
+delta_time = 0.01
+
+
+def get_pos_delta(pos_d, pos_up):
+    """подсчитываем разницу координат и длину вектора"""
+    radius = round(((-pos_d[0] + pos_up[0]) ** 2 + (-pos_d[1] + pos_up[1]) ** 2) ** 0.5, 4)
+    # if radius == 0:
+    #     radius = 0.00001
+    return (-pos_d[0] + pos_up[0], -pos_d[1] + pos_up[1], radius)
+
+
+def update_pos(pos, speed):
+    return (pos[0] + speed[0], pos[1] + speed[1])
+
+
+g_const = 0.1
+
+
+def update_speed(mass_point, cur_point):
+    a_x, a_y = 0, 0
+
+    abc_x, abc_y = cur_point[0]
+    if abc_x > weght or abc_x < 0:
+        cur_point[1] = -1 * math.copysign(cur_point[1], abc_x)
+        return 0, 0
+    if abc_y > heght or abc_y < 0:
+        cur_point[2] =  -1 * math.copysign(cur_point[2], abc_y)
+        return 0, 0
+
+
+    def leght_r(pos_0, pos_i):
+        return round(((pos_0[0] - pos_i[0]) ** 2 + (pos_0[1] - pos_i[1]) ** 2) ** 0.5, 0)
+
+    for point in mass_point:
+        if point != cur_point:
+            ## TODO: d_x, d_y, r
+            d_x, d_y, radius = get_pos_delta(cur_point[0], point[0])
+            # TODO:f_x, f_y
+            if radius <= 5:
+                if cur_point[3] > point[3]:
+                    cur_point[3] += point[3]
+                    # cur_point[1], cur_point[2] = 0, 0
+                    mass_point.remove(point)
+                    screen.fill(BLACK)
+                    continue
+                else:
+                    point[3] += cur_point[3]
+                    mass_point.remove(cur_point)
+                    screen.fill(BLACK)
+                    return 0,0
+
+
+            Force = point[3] * cur_point[3] * g_const / (radius ** 2)
+            f_x, f_y = Force * d_x / radius, Force * d_y / radius
+
+            # TODO: a-x,a-y
+            a_x += f_x / cur_point[3]
+            a_y += f_y / cur_point[3]
+            # TODO:v-x, v-y
+
+    return a_x * delta_time, a_y * delta_time
+
+
+sleep_u = 0
+while True:
+    pygame.time.delay(30)
+    if datetime.datetime.now().second // 2 == 0 and sleep_u == 0:
+        # time.sleep(0.1)
+        # screen.fill(BLACK)
+        print(mass_point)
+        sleep_u = 1
+        continue
+    if datetime.datetime.now().second // 2 != 0 and sleep_u == 1:
+        sleep_u = 0
+    # timer += 0.2
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos_up = pygame.mouse.get_pos()
+            mass_p = 1000 + round(9000 * random.random(), 10)
+            speed_x, speed_y = get_pos_delta(pos_d, pos_up)[:2]
+            mass_point.append([pos_d, speed_x * 0.001, speed_y * 0.001, mass_p])
+            # pygame.draw.line(screen, (100, 250, 100), pos_d, pos_up, 2)
+            print(mass_point)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos_d = pygame.mouse.get_pos()
+            pygame.draw.circle(screen, RED, pos_d, 5, 5)
+    if len(mass_point) > 0:
+        for point in mass_point:
+            pygame.draw.circle(screen, BLACK, point[0], 5, 5)
+            point[0] = update_pos(pos=point[0], speed=(point[1], point[2]))
+            pygame.draw.circle(screen, RED, point[0], 5, 5)
+    if len(mass_point) > 1:
+        for point in mass_point:
+            dx, dy = update_speed(mass_point, point)
+            point[1] += dx
+            point[2] += dy
+
+    pygame.display.update()
+
+exit()
